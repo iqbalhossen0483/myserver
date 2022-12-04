35e0da657829634a84cde9078e0e8d805429c960
unknown <rayhan.senbag.bd@gmail.com>
2022-12-04 20:20:35 +0600

    first

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const { mongoDb } = require("./mongoDb");
+const serviceRouter = require("./router/services");
+const gallaryRouter = require("./router/gallery");
+const orderRouter = require("./router/order");
+const blogRouter = require("./router/blog");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+//database
+const client = mongoDb();
+
+async function run() {
+  try {
+    await client.connect();
+
+    //services
+    app.use("/services", serviceRouter);
+
+    //gallery
+    app.use("/gallery", gallaryRouter);
+
+    //order
+    app.use("/orders", orderRouter);
+
+    //blog
+    app.use("/blogs", blogRouter);
+  } finally {
+    // client.close()
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send(`server is running in, ${port}`);
+});
+app.listen(port, () => {
+  console.log("its running", port);
+});
