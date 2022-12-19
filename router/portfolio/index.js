f3b0540837efb90181dd78ddb31666d4e1d576d5
unknown <rayhan.senbag.bd@gmail.com>
2022-12-19 13:08:27 +0600

    nodemailer

--- router/portfolio/index.js
+++ router/portfolio/index.js
@@ -1,46 +1,160 @@
 const express = require("express");
-const { mongoDb } = require("../../mongoDb");
-const { ObjectId } = require("mongodb");
-
-const client = mongoDb();
+const nodemailer = require("nodemailer");
 
 const prortfolio = express.Router();
 
-const run = async () => {
+//nodemailer;
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+prortfolio.post("/users", async (req, res, next) => {
   try {
-    await client.connect();
-    const database = client.db("porfolio");
-    const users = database.collection("contacts");
-
-    prortfolio.post("/users", async (req, res) => {
-      const user = req.body;
-      const result = await users.insertOne(user);
-      res.send(result);
-    });
-    prortfolio.get("/users", async (req, res) => {
-      const result = await users.find({}).toArray();
-      res.send(result);
-    });
-    prortfolio.delete("/users/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await users.deleteOne(query);
-      res.send(result);
-    });
-    prortfolio.put("/users", async (req, res) => {
-      const data = req.body;
-      const query = { _id: ObjectId(data.id) };
-      const update = {
-        $set: {
-          status: data.status,
-        },
-      };
-      const result = await users.updateOne(query, update);
-      res.send(result);
-    });
-  } finally {
+    const mailOptionsForClient = {
+      from: "rayhan.senbag.bd@gmail.com",
+      to: req.body.email,
+      subject: "Message received",
+      html: clientBody,
+    };
+    const mailOptionsForMe = {
+      from: "rayhan.senbag.bd@gmail.com",
+      to: "iqbal.hossen.senbag.bd@gmail.com",
+      subject: "From your portfolio",
+      html: myBody(req),
+    };
+    const client = await transporter.sendMail(mailOptionsForClient);
+    await transporter.sendMail(mailOptionsForMe);
+    if (client.messageId) {
+      res.send({ message: "success" });
+    } else next({ message: "Failed to received" });
+  } catch (error) {
+    next(error);
   }
-};
-run();
+});
 
+//mail body
+const clientBody = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <style>
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+      body {
+        font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+          "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+      }
+      #root {
+        width: 100%;
+        background-color: #00ffff1b;
+        padding: 10px 15px;
+        border-radius: 5px;
+        margin: 0;
+      }
+      @media (min-width: 768px) {
+        #root {
+          width: 600px;
+          margin: 0 auto;
+          margin-top: 5px;
+          margin-bottom: 5px;
+          background-color: #00ffff1b;
+          padding: 10px 15px;
+          border-radius: 5px;
+        }
+      }
+    </style>
+  </head>
+  <body>
+    <div id="root">
+      <header>
+        <h4 style="text-align: center">Thank you for contacting me!</h4>
+      </header>
+      <main style="margin-top: 15px">
+        <p style="margin-bottom: 5px">Dear <b>Md kader</b></p>
+        <p>
+          I appreciate you reaching out to me. I have found your email and will
+          contact you as soon as possible.
+        </p>
+        <p style="margin-top: 10px">Thank you vary much</p>
+        <p>Md Iqbal Hossen</p>
+      </main>
+    </div>
+  </body>
+</html>`;
+
+function myBody(req) {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <style>
+        * {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+        }
+        body {
+          font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+            "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+        }
+        #root {
+          width: 100%;
+          background-color: #00ffff1b;
+          padding: 10px 15px;
+          border-radius: 5px;
+          margin: 0;
+        }
+        @media (min-width: 768px) {
+          #root {
+            width: 600px;
+            margin: 0 auto;
+            margin-top: 5px;
+            margin-bottom: 5px;
+            background-color: #00ffff1b;
+            padding: 10px 15px;
+            border-radius: 5px;
+          }
+        }
+      </style>
+    </head>
+    <body>
+      <div id="root">
+        <header>
+          <p>
+            This is sent from your portfolio site By
+            <b>${req.body.name}</b>
+          </p>
+          <small>Time: ${new Date().toISOString()}</small>
+        </header>
+        <main style="margin-top: 10px">
+          <p>
+            <b>Email: </b>
+            ${req.body.email}
+          </p>
+          <p>
+            <b>Number: </b>
+            ${req.body.number}
+          </p>
+          <p style="margin-top: 5px">
+            <b>Message: </b>
+            ${req.body.message}
+          </p>
+        </main>
+      </div>
+    </body>
+  </html>`;
+}
 module.exports = prortfolio;
