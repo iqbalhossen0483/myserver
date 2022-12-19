d439251bcee72b71a04d6ebf7b9d314245ad1504
unknown <rayhan.senbag.bd@gmail.com>
2022-12-19 13:17:16 +0600

    sdf

--- router/portfolio/index.js
+++ router/portfolio/index.js
@@ -18,7 +18,7 @@ prortfolio.post("/users", async (req, res, next) => {
       from: "rayhan.senbag.bd@gmail.com",
       to: req.body.email,
       subject: "Message received",
-      html: clientBody,
+      html: clientBody(req),
     };
     const mailOptionsForMe = {
       from: "rayhan.senbag.bd@gmail.com",
@@ -37,60 +37,62 @@ prortfolio.post("/users", async (req, res, next) => {
 });
 
 //mail body
-const clientBody = `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <style>
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-      }
-      body {
-        font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
-          "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
-      }
-      #root {
-        width: 100%;
-        background-color: #00ffff1b;
-        padding: 10px 15px;
-        border-radius: 5px;
-        margin: 0;
-      }
-      @media (min-width: 768px) {
+function clientBody(req) {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <style>
+        * {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+        }
+        body {
+          font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+            "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+        }
         #root {
-          width: 600px;
-          margin: 0 auto;
-          margin-top: 5px;
-          margin-bottom: 5px;
+          width: 100%;
           background-color: #00ffff1b;
           padding: 10px 15px;
           border-radius: 5px;
+          margin: 0;
         }
-      }
-    </style>
-  </head>
-  <body>
-    <div id="root">
-      <header>
-        <h4 style="text-align: center">Thank you for contacting me!</h4>
-      </header>
-      <main style="margin-top: 15px">
-        <p style="margin-bottom: 5px">Dear <b>Md kader</b></p>
-        <p>
-          I appreciate you reaching out to me. I have found your email and will
-          contact you as soon as possible.
-        </p>
-        <p style="margin-top: 10px">Thank you vary much</p>
-        <p>Md Iqbal Hossen</p>
-      </main>
-    </div>
-  </body>
-</html>`;
+        @media (min-width: 768px) {
+          #root {
+            width: 600px;
+            margin: 0 auto;
+            margin-top: 5px;
+            margin-bottom: 5px;
+            background-color: #00ffff1b;
+            padding: 10px 15px;
+            border-radius: 5px;
+          }
+        }
+      </style>
+    </head>
+    <body>
+      <div id="root">
+        <header>
+          <h4 style="text-align: center">Thank you for contacting me!</h4>
+        </header>
+        <main style="margin-top: 15px">
+          <p style="margin-bottom: 5px">Dear <b>${req.body.name}</b></p>
+          <p>
+            I appreciate you reaching out to me. I have found your email and will
+            contact you as soon as possible.
+          </p>
+          <p style="margin-top: 10px">Thank you vary much</p>
+          <p>Md Iqbal Hossen</p>
+        </main>
+      </div>
+    </body>
+  </html>`;
+}
 
 function myBody(req) {
   return `
@@ -139,19 +141,10 @@ function myBody(req) {
           </p>
           <small>Time: ${new Date().toISOString()}</small>
         </header>
-        <main style="margin-top: 10px">
-          <p>
-            <b>Email: </b>
-            ${req.body.email}
-          </p>
-          <p>
-            <b>Number: </b>
-            ${req.body.number}
-          </p>
-          <p style="margin-top: 5px">
-            <b>Message: </b>
-            ${req.body.message}
-          </p>
+        <main style="margin-top: 20px">
+          <p>Email: ${req.body.email}</p>
+          <p>Number: ${req.body.number}</p>
+          <p style="margin-top: 5px">Message: ${req.body.message}</p>
         </main>
       </div>
     </body>
